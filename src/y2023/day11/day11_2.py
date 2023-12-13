9e98d2e96b322c00b9dd7ed097517b82cc8e1c24
tsironneau <tanguy.sironneau@gmail.com>
2023-12-13 19:34:21 +0100

    puzzle(2023): add day 11

---
+++ src/y2023/day11/day11_2.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+
+
+def emptyLines(lines):
+    res = []
+    for i in range(len(lines)):
+        if len(set(lines[i])) == 1:
+            res.append(i)
+    return res
+
+
+def findGalaxies(lines):
+    res = []
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == '#':
+                res.append((i, j))
+    return res
+
+
+def bonusSteps(mi, ma, empty):
+    res = 0
+    for e in empty:
+        if mi <= e <= ma:
+            res += 1000000-1
+    return res
+
+
+def pathLength(g1, g2, emptyL, emptyC):
+    g1x = g1[0]
+    g2x = g2[0]
+    g1y = g1[1]
+    g2y = g2[1]
+    manh = abs(g1x - g2x) + abs(g1y - g2y)
+
+    manh += bonusSteps(min(g1x, g2x), max(g1x, g2x), emptyL)
+    manh += bonusSteps(min(g1y, g2y), max(g1y, g2y), emptyC)
+
+    return manh
+
+
+def solution():
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+        emptyL = emptyLines(lines)
+        emptyC = emptyLines(list(zip(*lines)))
+
+        galaxies = findGalaxies(lines)
+
+        res = 0
+        for i in range(len(galaxies) - 1):
+            for j in range(i + 1, len(galaxies)):
+                g1 = galaxies[i]
+                g2 = galaxies[j]
+                res += pathLength(g1, g2, emptyL, emptyC)
+
+        return res
+
+
+if __name__ == '__main__':
+    print(solution())
